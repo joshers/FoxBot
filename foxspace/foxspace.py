2b26fc78f1ced4db1d179016d5c20052694dc48e
josh <josh@darkfox.io>
2019-11-08 15:14:10 -0500

    Add API calls

--- foxspace/foxspace.py
+++ foxspace/foxspace.py
@@ -10,13 +10,16 @@ class FoxSpace(commands.Cog):
     @commands.command()
     async def nextlaunch(self, ctx):
         """Displays Next Rocket Launch from LaunchLibrary API"""
-        # Then initialize an API object
+        api = ll.Api(retries=10)     
+        next_launch = ll.Launch.next(api, 1)
+        launch_loc = next_launch[0].location
+        launch_name = next_launch[0].name
         embed = discord.Embed(
             title="Next Launch", description="Upcoming Rocket Launch Information", color=0xFF0000
         )
-        embed.add_field(name="Rocket", value="Falcon 9 Block 5 | Starlink 1")
+        embed.add_field(name="Mission", value=launch_name)
         embed.add_field(name="Launch Window Begin", value="November 11, 2019 14:51:00 UTC")
         embed.add_field(name="Launch Window End", value="November 11, 2019 15:02:00 UTC")
-        embed.add_field(name="Launch Pad", value="Space Launch Complex 40, Cape Canaveral, FL")
+        embed.add_field(name="Launch Pad", value=launch_loc)
 
         await ctx.send(embed=embed)
