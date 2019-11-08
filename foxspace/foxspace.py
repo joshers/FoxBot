dbfdd551fdb5348b7a5df2e22fa278ee5a523caa
Josh Kosloski <josh@darkfox.io>
2019-11-08 18:32:32 -0500

    add color changing, start and end launch window times

--- foxspace/foxspace.py
+++ foxspace/foxspace.py
@@ -12,14 +12,23 @@ class FoxSpace(commands.Cog):
         """Displays Next Rocket Launch from LaunchLibrary API"""
         api = ll.Api(retries=10)     
         next_launch = ll.Launch.next(api, 1)
-        launch_loc = next_launch[0].pad
+        launch_loc = next_launch[0].location
+        launch_start = next_launch[0].windowstart
+        launch_end = next_launch[0].windowend
         launch_name = next_launch[0].name
+        launch_status = next_launch[0].status
+        if launch_status == "1":
+            status = "Green"
+            color = 0x00FF00
+        if launch_status == "2":
+            status = "Red"
+            color = 0xFF0000
         embed = discord.Embed(
-            title="Next Launch", description="Upcoming Rocket Launch Information", color=0xFF0000
+            title="Next Launch", description=launch_name, color=color
         )
-        embed.add_field(name="Mission", value=launch_name)
-        embed.add_field(name="Launch Window Begin", value="November 11, 2019 14:51:00 UTC")
-        embed.add_field(name="Launch Window End", value="November 11, 2019 15:02:00 UTC")
-        embed.add_field(name="Launch Pad", value=launch_loc)
+        embed.add_field(name="Status", value=launch_status)
+        embed.add_field(name="Window Begin", value=launch_start)
+        embed.add_field(name="Window End", value=launch_end)
+        embed.add_field(name="Pad", value=launch_loc)
 
         await ctx.send(embed=embed)
