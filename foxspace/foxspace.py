377909bc180ad2fea8a37bb670c790e18c1526f9
josh <josh@darkfox.io>
2019-11-08 14:47:44 -0500

    fix foxspace issue

--- foxspace/foxspace.py
+++ foxspace/foxspace.py
@@ -1,10 +1,21 @@
 from redbot.core import commands
 
+import launchlibrary as ll
+import asyncio
+
 class FoxSpace(commands.Cog):
-    """My custom cog"""
+    """FoxSpace Commands"""
 
     @commands.command()
     async def nextlaunch(self, ctx):
-        """This does stuff!"""
-        # Your code will go here
-        await ctx.send("Next Rocket Launch Will Display Here")
+        """Displays Next Rocket Launch from LaunchLibrary API"""
+        # Then initialize an API object
+		embed = discord.Embed(
+				title="Next Launch", description="Upcoming Rocket Launch Information", color="red"
+				)
+		embed.add_field(name="Rocket", value="Falcon 9 Block 5 | Starlink 1")
+		embed.add_field(name="Launch Window Begin", value="November 11, 2019 14:51:00 UTC")
+		embed.add_field(name="Launch Window End", value="November 11, 2019 15:02:00 UTC")
+		embed.add_field(name="Launch Pad", value="Space Launch Complex 40, Cape Canaveral, FL")
+
+        await ctx.send(embed=embed)
