ec4e2c4b4b6ca3e263993274124267ab928a19bd
josh <josh@darkfox.io>
2019-11-08 14:27:20 -0500

    edit ll

--- foxspace.py
+++ foxspace.py
@@ -8,19 +8,6 @@ class FoxSpace(commands.Cog):
 
     @commands.command()
     async def nextlaunch(self, ctx):
-        """Displays Next Rocket Launch from LaunchLibrary API"""
-        # Then initialize an API object
-		api = ll.Api()
-
-		# Get an event loop
-		event_loop = asyncio.get_event_loop()
-
-		# And fetch using the Async models.
-		nextLaunch = event_loop.run_until_complete(ll.AsyncLaunch.next(api, 1))
-
-		# Note that all API facing methods are now asynchronous, so you must await all of them.
-		status = event_loop.run_until_complete(nextLaunch[0].get_status())
-
 		embed = discord.Embed(
 				title="Next Launch", description="Upcoming Rocket Launch Information", color="red"
 				)
