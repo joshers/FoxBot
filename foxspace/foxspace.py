f8ee63c104f43cc350c21d86c97bceafa7fdbb08
josh <josh@darkfox.io>
2019-11-08 14:58:21 -0500

    fix file location

--- foxspace/foxspace.py
+++ foxspace/foxspace.py
@@ -10,9 +10,7 @@ class FoxSpace(commands.Cog):
     async def nextlaunch(self, ctx):
         """Displays Next Rocket Launch from LaunchLibrary API"""
         # Then initialize an API object
-		embed = discord.Embed(
-				title="Next Launch", description="Upcoming Rocket Launch Information", color="red"
-				)
+		embed = discord.Embed(title="Next Launch", description="Upcoming Rocket Launch Information", color="red")
 		embed.add_field(name="Rocket", value="Falcon 9 Block 5 | Starlink 1")
 		embed.add_field(name="Launch Window Begin", value="November 11, 2019 14:51:00 UTC")
 		embed.add_field(name="Launch Window End", value="November 11, 2019 15:02:00 UTC")
